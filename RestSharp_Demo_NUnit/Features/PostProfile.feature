6e5f13b3275603083d6ad781d37eef2d47cc01b0
Luke Menard <lukemenard@gmail.com>
2020-12-08 14:36:45 -0700

    added specflow test for POST operation

---
+++ RestSharp_Demo_NUnit/Features/PostProfile.feature
@@ -0,0 +1,9 @@
+﻿Feature: PostProfile
+	Test POST operation using Rest-assured library
+	
+@smoke
+Scenario: Verify Post operation for Profile
+	Given I Perform Post operation for "/posts/{profileNo}/profile" with body
+		| name | profile |
+		| Sams | 2		 |
+	Then I should see the "name" name as "Sams"
---
+++ RestSharp_Demo_NUnit/Steps/PostProfileSteps.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using RestSharp;
+using RestSharp_Demo_NUnit.Base;
+using RestSharp_Demo_NUnit.Model;
+using RestSharp_Demo_NUnit.Utilities;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+
+namespace RestSharp_Demo_NUnit.Steps
+{
+    [Binding]
+    public class PostProfileSteps
+    {
+
+        private Settings _settings;
+        public PostProfileSteps(Settings settings) => _settings = settings;
+
+        [Given(@"I Perform POST operation for ""(.*)"" with body")]
+        public void GivenIPerformPOSTOperationForWithBody(string url, Table table)
+        {
+            dynamic data = table.CreateDynamicInstance();
+
+            _settings.Request = new RestRequest(url, Method.POST);
+
+            _settings.Request.AddJsonBody(new { name = data.name.ToString() });
+            _settings.Request.AddUrlSegment("profileNo", ((int)data.profile).ToString());
+
+            _settings.Response = _settings.RestClient.ExecuteAsyncRequest<Posts>(_settings.Request).GetAwaiter().GetResult();
+        }
+
+
+    }
+}
