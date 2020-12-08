133b08308b3f92d56101c0983bc55a4c6ed357f2
Luke Menard <lukemenard@gmail.com>
2020-12-07 20:05:26 -0700

    added postWithAsync test

--- RestSharp_Demo_NUnit/UnitTest1.cs
+++ RestSharp_Demo_NUnit/UnitTest1.cs
@@ -5,6 +5,7 @@ using RestSharp.Serialization.Json;
 using NUnit.Framework;
 using Newtonsoft.Json.Linq;
 using RestSharp_Demo_NUnit.Model;
+using System.Threading.Tasks;
 
 namespace RestSharp_Demo_NUnit
 {
@@ -58,7 +59,7 @@ namespace RestSharp_Demo_NUnit
 
             var request = new RestRequest("posts", Method.POST);
 
-            request.AddJsonBody(new Posts() { id = "20", author = "Execute Automation", title = "RestSharp demo course" });
+            request.AddJsonBody(new Posts() { id = "24", author = "Execute Automation", title = "RestSharp demo course" });
 
             var response = client.Execute<Posts>(request);
 
@@ -69,6 +70,38 @@ namespace RestSharp_Demo_NUnit
             Assert.That(response.Data.author, Is.EqualTo("Execute Automation"), "Author is not correct");
         }
 
+        [Test]
+        public void PostWithAsync()
+        {
+            var client = new RestClient("http://localhost:3000/");
 
+            var request = new RestRequest("posts", Method.POST);
+
+            request.AddJsonBody(new Posts() { id = "25", author = "Execute Automation", title = "RestSharp demo course" });
+
+            //var response = client.ExecuteAsync<Posts>(request);
+
+            var response = ExecuteAsyncRequest<Posts>(client, request).GetAwaiter().GetResult();
+
+            Assert.That(response.Data.author, Is.EqualTo("Execute Automation"), "Author is not correct");
+        }
+
+        private async Task<IRestResponse<T>> ExecuteAsyncRequest<T>(RestClient client, IRestRequest request) where T: class, new()
+        {
+            var taskCompletionSource = new TaskCompletionSource<IRestResponse<T>>();
+
+            client.ExecuteAsync<T>(request, restResponse =>
+            {
+                if (restResponse.ErrorException != null)
+                {
+                    const string message = "Error retrieving response.";
+                    throw new ApplicationException(message, restResponse.ErrorException);
+                }
+
+                taskCompletionSource.SetResult(restResponse);
+            });
+
+            return await taskCompletionSource.Task;
+        }
     }
 }
