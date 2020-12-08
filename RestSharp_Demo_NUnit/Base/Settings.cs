9945376ec1de7c2227ff6e46b80b58c1f02427bc
Luke Menard <lukemenard@gmail.com>
2020-12-08 13:55:48 -0700

    added context injection

---
+++ RestSharp_Demo_NUnit/Base/Settings.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace RestSharp_Demo_NUnit.Base
+{
+    public class Settings
+    {
+        public Settings()
+        {
+        }
+    }
+}
--- RestSharp_Demo_NUnit/Steps/GetPostsSteps.cs
+++ RestSharp_Demo_NUnit/Steps/GetPostsSteps.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using NUnit.Framework;
 using RestSharp;
+using RestSharp_Demo_NUnit.Base;
 using RestSharp_Demo_NUnit.Model;
 using RestSharp_Demo_NUnit.Utilities;
 using TechTalk.SpecFlow;
@@ -10,27 +11,29 @@ namespace RestSharp_Demo_NUnit.Steps
     [Binding]
     public class GetPostsSteps
     {
-        public RestClient client = new RestClient("http://localhost:3000/");
-        public RestRequest request = new RestRequest();
-        public IRestResponse<Posts> response = new RestResponse<Posts>();
+        private Settings _settings;
+        public GetPostsSteps(Settings settings) => _settings = settings;
+
+        //public RestClient client = new RestClient("http://localhost:3000");
 
         [Given(@"I perform GET operation for ""(.*)""")]
         public void GivenIPerformGETOperationFor(string url)
         {
-            request = new RestRequest(url, Method.GET);
+            _settings.RestClient.BaseUrl = new System.Uri("http://localhost:3000");
+            _settings.Request = new RestRequest(url, Method.GET);
         }
 
         [Given(@"I perform operation for post ""(.*)""")]
         public void GivenIPerformOperationForPost(int postId)
         {
-            request.AddUrlSegment("postid", postId.ToString());
-            client.ExecuteAsyncRequest<Posts>(request).GetAwaiter().GetResult();
+            _settings.Request.AddUrlSegment("postid", postId.ToString());
+            _settings.Response = _settings.RestClient.ExecuteAsyncRequest<Posts>(_settings.Request).GetAwaiter().GetResult();
         }
 
         [Then(@"I should see the ""(.*)"" name as ""(.*)""")]
         public void ThenIShouldSeeTheNameAs(string key, string value)
         {
-            Assert.That(response.GetResponseObject(key), Is.EqualTo(value), $"The {key} is not matching");
+            Assert.That(_settings.Response.GetResponseObject(key), Is.EqualTo(value), $"The {key} is not matching");
         }
     }
 }
